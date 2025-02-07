66e5c1b70652dd35fcd55d407885ab291a4e34a9
Kevin Payan <125767404+Kevin-Payan@users.noreply.github.com>
2025-02-06 16:34:41 -0800

    about me responsive

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -17,12 +17,27 @@ import awsImage from "../assets/skills/aws.svg";
 import firebaseImage from "../assets/skills/firebase.svg";
 
 const About = () => {
+
+  const skills = [
+    { name: "Python", image: pythonImage },
+    { name: "JavaScript", image: javascriptImage },
+    { name: "Tailwind CSS", image: tailwindImage },
+    { name: "React", image: reactImage },
+    { name: "Node.js", image: nodeImage },
+    { name: "Express.js", image: expressImage },
+    { name: "SQL", image: sqlImage },
+    { name: "MongoDB", image: mongodbImage },
+    { name: "Git", image: gitImage },
+    { name: "AWS", image: awsImage },
+    { name: "Firebase", image: firebaseImage }
+  ];
+
   return (
     <div className="w-screen h-auto m-5 p-5 bg-accent" id="about">
       <h1 className="text-white font-semibold text-4xl m-3 p-3 underline decoration-secondary underline-offset-4 font-title">
         About Me
       </h1>
-      <div className="text-white text-lg m-3 mb-14 p-3 rounded-md bg-card italic bg-opacity-30">
+      <div className="text-white text-lg m-3 mb-14 md:mx-6 p-3 rounded-md bg-card italic bg-opacity-30">
         <p>
           "Building the future, one line of code at a time. As a Software
           Engineer with a passion for{" "}
@@ -34,25 +49,17 @@ const About = () => {
       <h1 className="text-white font-semibold text-4xl text-center m-4 font-title">
         My Skills
       </h1>
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex justify-center items-center">
-          <Card name="Python" image={pythonImage} />
-          <Card name="JavaScript" image={javascriptImage} />
-          <Card name="Tailwind CSS" image={tailwindImage} />
-          <Card name="React" image={reactImage} />
-        </div>
-        <div className="flex justify-center items-center">
-          <Card name="Node.js" image={nodeImage} />
-          <Card name="Express.js" image={expressImage} />
-          <Card name="SQL" image={sqlImage} />
-          <Card name="MongoDB" image={mongodbImage} />
-        </div>
-        <div className="flex justify-center items-center">
-          <Card name="Git" image={gitImage} />
-          <Card name="AWS" image={awsImage} />
-          <Card name="Firebase" image={firebaseImage} />
-        </div>
+      <div className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 max-w-6xl mx-auto">
+        {skills.map((skill) => (
+          <Card 
+            key={skill.name}
+            name={skill.name}
+            image={skill.image}
+          />
+        ))}
       </div>
+    </div>
     </div>
   );
 };
